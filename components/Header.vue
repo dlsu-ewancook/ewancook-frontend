cbba9fbfb775b9c5924f36e1aa471dc602680493
jolocansana <jcansana@gmail.com>
2022-06-13 21:30:19 +0800

    imported font, added header

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<style src="../assets/css/header.css"></style>
+<template>
+  <div class="bg-white py-4 px-5">
+    <span class="header_title">ewancook.</span>
+  </div>
+</template>
--- pages/search/add_ingredients.vue
+++ pages/search/add_ingredients.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-    <div>
-        Add Ingredients
+    <Header />
+    <div class="container">
+      <div class="row">
+        <div class="col-9 bg-success">Left</div>
+        <div class="col-3 bg-warning">Right</div>
+      </div>
     </div>
 </template>
