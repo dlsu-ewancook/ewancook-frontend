e28d37659221e9c47695386ebb2d50d3acb7ef3f
Trisha <48178740+trishapelagio@users.noreply.github.com>
2022-06-16 20:10:27 +0800

    landing page done

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,70 @@
 </script>
 
 <template>
-    <div>
+    <div class="container" style="margin-top:0px">
         <Header />
-        <b-card
-        overlay
-        img-src=""
-        img-alt="Card Image"
-        text-variant="white"
-        title="Image Overlay"
-        sub-title="Subtitle">
-            <h2>Streamline your<br>home cooking with</h2>
-            <h1>ewancook.</h1>
+        <div>
+            <div class="row">
+                <div class="col-md-6" style="background-color:#ED6A32; margin-top:0px;">
+                    <div class="row" style="margin:50px;">
+                        <div class="col-md-12">
+                            <div class="row" style="margin-top:30px">
+                                <h2 class="texts" style="font-size:40px">Streamline your<br>home cooking with</h2>
+                            </div>
+                            <div class="row" style="margin-top:-5px">
+                                <h1 class="texts" style="font-size:60px">ewancook.</h1>
+                            </div>
+                            <div class="row" style="margin-top:30px">
+                                <h3 class="texts-nb" style="font-size:30px">Search for recipes based on what<br>you have instead of shoppping for<br>your recipe.</h3>
+                            </div>
 
-            <h3>Search for recipes based on what<br>you have instead of shopping for<br>your recipe.</h3>
+                            <button type="button" href="\add_ingredients" class="btn btn-outline-light texts-nb" style="font-size:20px; margin-top:30px; margin-bottom:50px;">Search With Your Ingredients</button>
+                        </div>
+                    </div>    
+                </div>
+                <div class="col-md-6">
+                    <img src="pages/images/cooking.jpg" style="object-fit: cover; width:100%; height:100%; z-index: 0;">
+                </div>
+            </div> 
+        </div>
 
-            <b-button variant="outline-light">Search With Your Ingredients</b-button>
-        </b-card>
+        <div style="background-color:white; width:100%; height:100%; margin-top:-60px">
+            <div class="row" style="margin:50px;">
+                <div class="col-md-12">
+                    <div class="row" style="margin-top:50px;">
+                        <h4 class="texts-below" style="font-size:20px">ewancook.</h4>
+                    </div>
+                    <div class="row" style="margin-top:-5px;">
+                        <h3 class="texts-nb-below" style="font-size:15px;">Transforming the way you cook.</h3>
+                    </div>
+                </div>
+            </div>   
+        </div>
+        
     </div>
 </template>
+
+<style>
+    .texts{
+        color: white;
+        font-weight: bold;
+        font-family: 'Quicksand', sans-serif;
+    }
+
+    .texts-below{
+        color: black;
+        font-weight: bold;
+        font-family: 'Quicksand', sans-serif;
+    }
+
+    .texts-nb{
+        color: white;
+        font-family: 'Quicksand', sans-serif;
+    }
+
+    .texts-nb-below{
+        color: black;
+        font-family: 'Quicksand', sans-serif;
+    }
+
+</style>
